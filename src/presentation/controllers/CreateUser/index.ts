82c1ed3189993745677e4879ec02bb224a5d8e71
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-01-10 06:50:06 -0300

    feat: implements CreateUserController with handling and tests

---
+++ src/presentation/controllers/CreateUser/index.ts
@@ -0,0 +1,21 @@
+import {
+  CreateUserRepository,
+  CreateUserRepositoryParams,
+} from 'app/repositories/CreateUser'
+import { Controller, HTTPErrorResponse, HTTPResponse } from '..'
+import { UnexpectedError } from 'app/errors/UnexpectedError'
+
+export class CreateUserController implements Controller {
+  constructor(private readonly createUserRepo: CreateUserRepository) {}
+
+  async handle(
+    params: CreateUserRepositoryParams,
+  ): Promise<HTTPResponse | HTTPErrorResponse> {
+    try {
+      const table = await this.createUserRepo.create(params)
+      return new HTTPResponse(table, 201)
+    } catch (error) {
+      return new HTTPErrorResponse(new UnexpectedError(), 500)
+    }
+  }
+}
---
+++ src/presentation/controllers/CreateUser/test.ts
@@ -0,0 +1,51 @@
+import {
+  mockCreateUserRepository,
+  mockCreateUserRepositoryParams,
+} from 'app/repositories/CreateUser/mock'
+import { CreateUserController } from '.'
+import { mockUser } from 'entities/User/mock'
+import { HTTPErrorResponse, HTTPResponse } from '..'
+import { UnexpectedError } from 'app/errors/UnexpectedError'
+
+const makeMocks = () => {
+  const createUserRepo = mockCreateUserRepository()
+  const sut = new CreateUserController(createUserRepo)
+
+  return { sut, createUserRepo }
+}
+
+describe('CreateUserController', () => {
+  it('should call create table repo with right params', async () => {
+    const { createUserRepo, sut } = makeMocks()
+
+    const params = mockCreateUserRepositoryParams()
+    await sut.handle(params)
+
+    expect(createUserRepo.create).toHaveBeenCalledWith(params)
+  })
+
+  it('should return 201 on success', async () => {
+    const { sut, createUserRepo } = makeMocks()
+    const table = mockUser()
+    createUserRepo.create.mockResolvedValueOnce(table)
+
+    const response = (await sut.handle(
+      mockCreateUserRepositoryParams(),
+    )) as HTTPResponse
+
+    expect(response.data).toBe(table)
+    expect(response.statusCode).toBe(201)
+  })
+
+  it('should return 500 on failure', async () => {
+    const { sut, createUserRepo } = makeMocks()
+    createUserRepo.create.mockRejectedValueOnce(new Error('any_error'))
+
+    const response = (await sut.handle(
+      mockCreateUserRepositoryParams(),
+    )) as HTTPErrorResponse
+
+    expect(response.error).toBeInstanceOf(UnexpectedError)
+    expect(response.statusCode).toBe(500)
+  })
+})
