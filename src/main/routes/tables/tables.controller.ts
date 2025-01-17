66c8b5b660c3f663a4c8c86212b35ca3eba0fceb
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-01-17 06:54:01 -0300

    feat: adds GetTables endpoint to TablesController with corresponding module integration

--- src/main/routes/tables/tables.controller.ts
+++ src/main/routes/tables/tables.controller.ts
@@ -1,14 +1,21 @@
-import { Body, Controller, Post, Res } from '@nestjs/common'
+import { Body, Controller, Get, Post, Query, Res } from '@nestjs/common'
 import { Response } from 'express'
 import {
   CreateTableController,
   CreateTableControllerParams,
 } from 'presentation/controllers/CreateTable'
+import {
+  GetTablesController,
+  GetTablesControllerParams,
+} from 'presentation/controllers/GetTables'
 import { HTTPResponse } from 'presentation/interfaces/Controller'
 
 @Controller('tables')
 export class TablesController {
-  constructor(private readonly createTableController: CreateTableController) {}
+  constructor(
+    private readonly createTableController: CreateTableController,
+    private readonly getTablesController: GetTablesController,
+  ) {}
 
   @Post()
   async create(
@@ -16,6 +23,20 @@ export class TablesController {
     @Res() res: Response,
   ) {
     const result = await this.createTableController.handle(params)
+
+    if (result instanceof HTTPResponse) {
+      return res.status(result.statusCode).json(result.data)
+    } else {
+      return res.status(result.error.statusCode).json({
+        name: result.error.name,
+        message: result.error.message,
+      })
+    }
+  }
+
+  @Get()
+  async get(@Query() params: GetTablesControllerParams, @Res() res: Response) {
+    const result = await this.getTablesController.handle(params)
 
     if (result instanceof HTTPResponse) {
       return res.status(result.statusCode).json(result.data)
--- src/main/routes/tables/tables.module.ts
+++ src/main/routes/tables/tables.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common'
 import { TablesController } from './tables.controller'
 import { CreateTableController } from 'presentation/controllers/CreateTable'
 import { makeCreateTableController } from 'presentation/controllers/CreateTable/factory'
+import { makeGetTablesController } from 'presentation/controllers/GetTables/factory'
+import { GetTablesController } from 'presentation/controllers/GetTables'
 
 @Module({
   controllers: [TablesController],
@@ -9,6 +11,10 @@ import { makeCreateTableController } from 'presentation/controllers/CreateTable/
     {
       provide: CreateTableController,
       useFactory: makeCreateTableController,
+    },
+    {
+      provide: GetTablesController,
+      useFactory: makeGetTablesController,
     },
   ],
 })
