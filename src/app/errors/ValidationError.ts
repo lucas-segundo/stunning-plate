53255eec88a0360ec6e13a8ef25ae4a8c2a61c1d
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-01-20 06:23:12 -0300

    feat: adds statusCode property to ValidationError for improved error handling

--- src/app/errors/ValidationError.ts
+++ src/app/errors/ValidationError.ts
@@ -3,10 +3,12 @@ import { KnownError } from './KnownError'
 export class ValidationError extends KnownError {
   code: string
   field: string
+  statusCode: number
 
   constructor(code: string, field: string, message: string) {
     super(message)
     this.code = code
     this.field = field
+    this.statusCode = 400
   }
 }
