278318f992d8bd9a7fae9d701e341c28f0b36472
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-01-22 07:08:44 -0300

    feat: adds logic to parse seats query strings to int

--- src/presentation/controllers/GetTables/index.ts
+++ src/presentation/controllers/GetTables/index.ts
@@ -10,7 +10,13 @@ import {
 import { UseErrorHandler } from 'presentation/decorators/ErrorHandler'
 import { Validation } from 'presentation/interfaces/Validation'
 
-export type GetTablesControllerParams = GetTablesRepositoryParams['where']
+export interface GetTablesControllerParams {
+  seats?: {
+    equals?: string
+    greaterThanOrEqual?: string
+    lessThanOrEqual?: string
+  }
+}
 
 export class GetTablesController implements Controller {
   constructor(
@@ -23,7 +29,29 @@ export class GetTablesController implements Controller {
     params: GetTablesControllerParams,
   ): Promise<HTTPResponse | HTTPErrorResponse> {
     await this.validation.validate(params)
-    const tables = await this.getTablesRepo.get({ where: params })
+    const tables = await this.getTablesRepo.get(this.adaptParams(params))
     return new HTTPResponse(tables, 200)
+  }
+
+  private adaptParams({
+    seats,
+  }: GetTablesControllerParams): GetTablesRepositoryParams {
+    return {
+      where: {
+        seats: this.adaptSeats(seats),
+      },
+    }
+  }
+
+  private adaptSeats(seats: GetTablesControllerParams['seats']) {
+    return {
+      equals: seats?.equals ? parseInt(seats.equals) : undefined,
+      greaterThanOrEqual: seats?.greaterThanOrEqual
+        ? parseInt(seats.greaterThanOrEqual)
+        : undefined,
+      lessThanOrEqual: seats?.lessThanOrEqual
+        ? parseInt(seats.lessThanOrEqual)
+        : undefined,
+    }
   }
 }
--- src/presentation/controllers/GetTables/test.ts
+++ src/presentation/controllers/GetTables/test.ts
@@ -1,47 +1,60 @@
-import {
-  mockGetTablesRepository,
-  mockGetTablesRepositoryParams,
-} from 'app/repositories/GetTables/mock'
-import { GetTablesController } from '.'
+import { mockGetTablesRepository } from 'app/repositories/GetTables/mock'
+import { GetTablesController, GetTablesControllerParams } from '.'
 import { mockTable } from 'entities/Table/mock'
 import { HTTPResponse } from '../../interfaces/Controller'
 import { mockValidation } from 'presentation/interfaces/Validation/mock'
+import { faker } from '@faker-js/faker/.'
+import { GetTablesRepositoryParams } from 'app/repositories/GetTables'
+
+const mockControllerParams = (): GetTablesControllerParams => ({
+  seats: {
+    equals: faker.number.int().toString(),
+    greaterThanOrEqual: faker.number.int().toString(),
+  },
+})
 
 const makeMocks = () => {
   const getTablesRepo = mockGetTablesRepository()
   const validation = mockValidation()
   const sut = new GetTablesController(getTablesRepo, validation)
+  const params = mockControllerParams()
 
-  return { sut, getTablesRepo, validation }
+  return { sut, getTablesRepo, validation, params }
 }
 
 describe('GetTablesController', () => {
   it('should call get tables repo with right params', async () => {
-    const { getTablesRepo, sut } = makeMocks()
+    const { getTablesRepo, sut, params } = makeMocks()
 
-    const params = mockGetTablesRepositoryParams().where
     await sut.handle(params)
 
-    expect(getTablesRepo.get).toHaveBeenCalledWith(params)
+    const expectedParams: GetTablesRepositoryParams = {
+      where: {
+        seats: {
+          equals: parseInt(params.seats!.equals!),
+          greaterThanOrEqual: parseInt(params.seats!.greaterThanOrEqual!),
+          lessThanOrEqual: undefined,
+        },
+      },
+    }
+
+    expect(getTablesRepo.get).toHaveBeenCalledWith(expectedParams)
   })
 
   it('should call validation with right params', async () => {
-    const { sut, validation } = makeMocks()
+    const { sut, validation, params } = makeMocks()
 
-    const params = mockGetTablesRepositoryParams().where
     await sut.handle(params)
 
     expect(validation.validate).toHaveBeenCalledWith(params)
   })
 
   it('should return 200 on success', async () => {
-    const { sut, getTablesRepo } = makeMocks()
+    const { sut, getTablesRepo, params } = makeMocks()
     const tables = [mockTable(), mockTable()]
     getTablesRepo.get.mockResolvedValueOnce(tables)
 
-    const response = (await sut.handle(
-      mockGetTablesRepositoryParams().where,
-    )) as HTTPResponse
+    const response = (await sut.handle(params)) as HTTPResponse
 
     expect(response.data).toBe(tables)
     expect(response.statusCode).toBe(200)
