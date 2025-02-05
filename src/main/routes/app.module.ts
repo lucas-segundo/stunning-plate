bbb280e689b40afb45189a03542ac7e9e2b86be1
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-02-05 11:43:16 -0300

    feat: adds AppController with a hello world endpoint

--- src/main/routes/app.module.ts
+++ src/main/routes/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common'
 import { UsersModule } from './users/users.module'
 import { TablesModule } from './tables/tables.module'
 import { BookingsModule } from './bookings/bookings.module'
+import { AppController } from './app.controller'
 
 @Module({
   imports: [UsersModule, TablesModule, BookingsModule],
+  controllers: [AppController],
 })
 export class AppModule {}
