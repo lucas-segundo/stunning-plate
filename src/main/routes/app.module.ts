3507c5f53290bd697cee11a7621d096ad3a36b45
Lucas Segundo <lucas.fernandes.app@gmail.com>
2025-02-19 08:54:11 -0300

    feat: adds Metrics module with controller and service for Prometheus metrics

--- src/main/routes/app.module.ts
+++ src/main/routes/app.module.ts
@@ -3,10 +3,17 @@ import { UsersModule } from './users/users.module'
 import { TablesModule } from './tables/tables.module'
 import { BookingsModule } from './bookings/bookings.module'
 import { AppController } from './app.controller'
-import { HealthModule } from './health/health.module';
+import { HealthModule } from './health/health.module'
+import { MetricsModule } from './metrics/metrics.module'
 
 @Module({
-  imports: [UsersModule, TablesModule, BookingsModule, HealthModule],
+  imports: [
+    UsersModule,
+    TablesModule,
+    BookingsModule,
+    HealthModule,
+    MetricsModule,
+  ],
   controllers: [AppController],
 })
 export class AppModule {}
